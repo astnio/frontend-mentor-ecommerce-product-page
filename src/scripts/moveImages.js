b4fffa1bdfc21dc54c61e95ed5c072230ac9815f
astn <github@msg.austinh.io>
2024-10-17 00:23:51 -0700

    Going wild and refactoring my rotating list logic

---
+++ src/scripts/moveImages.js
@@ -0,0 +1,12 @@
+const imageMainSources = [
+  '/src/assets/images/image-product-1.jpg',
+  '/src/assets/images/image-product-2.jpg',
+  '/src/assets/images/image-product-3.jpg',
+  '/src/assets/images/image-product-4.jpg',
+];
+
+export function handleOnItemClick() {}
+
+export function handleNextOnItemClick() {}
+
+export function handlePreviousOnItemClick() {}
