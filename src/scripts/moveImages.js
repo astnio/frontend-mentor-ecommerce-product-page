a0f033e6e1aaaeaec937eeb7d40af7d47fc4ac9c
astn <github@msg.austinh.io>
2024-10-17 09:06:25 -0700

    Didnt fix the issue, but sure did a lot of refactoring for nuthin

--- src/scripts/moveImages.js
+++ src/scripts/moveImages.js
@@ -1,30 +1,49 @@
-const imageMainSources = [
-  '/src/assets/images/image-product-1.jpg',
-  '/src/assets/images/image-product-2.jpg',
-  '/src/assets/images/image-product-3.jpg',
-  '/src/assets/images/image-product-4.jpg',
-];
+class ImagePicker {
+  static imageMainSources = [
+    '/src/assets/images/image-product-1.jpg',
+    '/src/assets/images/image-product-2.jpg',
+    '/src/assets/images/image-product-3.jpg',
+    '/src/assets/images/image-product-4.jpg',
+  ];
 
-let currentIndex = 0;
-let lastIndex = 0;
+  static overlayTracker = {
+    currentIndex: 0,
+    lastIndex: 0,
+  };
 
-function getIndexOfListItem(list, target) {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === target) {
-      return i;
+  static pageTracker = {
+    currentIndex: 0,
+    lastIndex: 0,
+  };
+
+  static getIndexOfListItem(list, target) {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === target) {
+        return i;
+      }
     }
   }
+
+  static updateItem(tracker, list, image, e) {
+    tracker.currentIndex = this.getIndexOfListItem(
+      list,
+      e.target.parentElement
+    );
+    list[tracker.lastIndex].setAttribute('data-active', 'false');
+    list[tracker.currentIndex].setAttribute('data-active', 'true');
+    tracker.lastIndex = tracker.currentIndex;
+
+    image.src = this.imageMainSources[tracker.currentIndex];
+  }
+
+  static handleOnItemClick(list, image, e, isOverlay) {
+    const tracker = isOverlay ? this.overlayTracker : this.pageTracker;
+    this.updateItem(tracker, list, image, e);
+  }
+
+  static handleNextOnItemClick() {}
+
+  static handlePreviousOnItemClick() {}
 }
 
-export function handleOnItemClick(list, image, e) {
-  currentIndex = getIndexOfListItem(list, e.target.parentElement);
-  list[lastIndex].setAttribute('data-active', 'false');
-  list[currentIndex].setAttribute('data-active', 'true');
-  lastIndex = currentIndex;
-
-  image.src = imageMainSources[currentIndex];
-}
-
-export function handleNextOnItemClick() {}
-
-export function handlePreviousOnItemClick() {}
+export default ImagePicker;
