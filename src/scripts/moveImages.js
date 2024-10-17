d44c6c7d5d8cb5a1a9d464746c1b7a26624c0b1f
astn <github@msg.austinh.io>
2024-10-17 00:37:35 -0700

    Kind of returned some functionality

--- src/scripts/moveImages.js
+++ src/scripts/moveImages.js
@@ -5,7 +5,25 @@ const imageMainSources = [
   '/src/assets/images/image-product-4.jpg',
 ];
 
-export function handleOnItemClick() {}
+let currentIndex = 0;
+let lastIndex = 0;
+
+function getIndexOfListItem(list, target) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === target) {
+      return i;
+    }
+  }
+}
+
+export function handleOnItemClick(list, image, e) {
+  currentIndex = getIndexOfListItem(list, e.target.parentElement);
+  list[lastIndex].setAttribute('data-active', 'false');
+  list[currentIndex].setAttribute('data-active', 'true');
+  lastIndex = currentIndex;
+
+  image.src = imageMainSources[currentIndex];
+}
 
 export function handleNextOnItemClick() {}
 
