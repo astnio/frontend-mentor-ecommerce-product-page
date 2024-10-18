8d1348e62f2cc879b6b7d0a322ab267022316210
astn <github@msg.austinh.io>
2024-10-18 15:38:33 -0700

    Number input can now add or subtract with buttons

--- src/scripts/cartManager.js
+++ src/scripts/cartManager.js
@@ -3,9 +3,34 @@ import { openCart } from './viewCartButton.js';
 const btnAddToCart = document.getElementById('btn-add-to-cart');
 const btnIncrementItem = document.getElementById('btn-item-increment');
 const btnDecrementItem = document.getElementById('btn-item-decrement');
+const inputItemQty = document.getElementById('item-quantity');
 
 export default function initAddToCartButton() {
   btnAddToCart.addEventListener('click', () => {
     openCart();
+  });
+
+  btnIncrementItem.addEventListener('click', () => {
+    if (!inputItemQty.value) {
+      inputItemQty.value = 0;
+    }
+
+    if (parseInt(inputItemQty.value) >= 100) {
+      inputItemQty.value = 100;
+    } else {
+      inputItemQty.value = parseInt(inputItemQty.value) + 1;
+    }
+  });
+
+  btnDecrementItem.addEventListener('click', () => {
+    if (!inputItemQty.value) {
+      inputItemQty.value = 0;
+    }
+
+    if (parseInt(inputItemQty.value) <= 0) {
+      inputItemQty.value = 0;
+    } else {
+      inputItemQty.value = parseInt(inputItemQty.value) - 1;
+    }
   });
 }
