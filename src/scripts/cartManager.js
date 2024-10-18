444040ebb1f24ebe25a0cfed80a2921e929a0c34
astn <github@msg.austinh.io>
2024-10-18 15:12:31 -0700

    Changed quantity to input and gave it a screen reader friendly label

--- src/scripts/cartManager.js
+++ src/scripts/cartManager.js
@@ -1,6 +1,8 @@
 import { openCart } from './viewCartButton.js';
 
 const btnAddToCart = document.getElementById('btn-add-to-cart');
+const btnIncrementItem = document.getElementById('btn-item-increment');
+const btnDecrementItem = document.getElementById('btn-item-decrement');
 
 export default function initAddToCartButton() {
   btnAddToCart.addEventListener('click', () => {
