7096730cade15f17eecb19a0e5ef6f785be069f5
astn <github@msg.austinh.io>
2024-10-18 08:44:12 -0700

    Setup some more stylings for cart menu

--- src/scripts/cartButton.js
+++ src/scripts/cartButton.js
@@ -3,15 +3,19 @@ const cartButton = document.getElementById('cart-button');
 
 console.log(cartMenu);
 
-let cartOpen = true;
+let cartOpen = false;
 
 function openCart() {
-  cartMenu.style.opacity = 0;
+  cartMenu.style.opacity = 1;
+  cartMenu.style.visibility = 'visible';
+  cartMenu.style.pointerEvents = 'all';
   cartOpen = true;
 }
 
 function closeCart() {
-  cartMenu.style.opacity = 1;
+  cartMenu.style.opacity = 0;
+  cartMenu.style.visibility = 'hidden';
+  cartMenu.style.pointerEvents = 'none';
   cartOpen = false;
 }
 
