f7804cee42354b02691b3042ebdfad9e09692b1c
astn <github@msg.austinh.io>
2024-10-18 14:51:49 -0700

    Removed unused console log

--- src/scripts/cartManager.js
+++ src/scripts/cartManager.js
@@ -4,7 +4,6 @@ const btnAddToCart = document.getElementById('btn-add-to-cart');
 
 export default function initAddToCartButton() {
   btnAddToCart.addEventListener('click', () => {
-    console.log('clicked');
     openCart();
   });
 }
