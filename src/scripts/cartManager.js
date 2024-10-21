989680f27f039aa5db1c5c06560b14fa80db8822
astn <github@msg.austinh.io>
2024-10-21 12:25:49 -0700

    Refactored code to be more concise

--- src/scripts/cartManager.js
+++ src/scripts/cartManager.js
@@ -1,36 +1,66 @@
 import { openCart } from './viewCartButton.js';
 
 const btnAddToCart = document.getElementById('btn-add-to-cart');
+const btnRemoveFromCart = document.getElementById('btn-delete-cart-item');
+
 const btnIncrementItem = document.getElementById('btn-item-increment');
 const btnDecrementItem = document.getElementById('btn-item-decrement');
 const inputItemQty = document.getElementById('item-quantity');
 
-export default function initAddToCartButton() {
-  btnAddToCart.addEventListener('click', () => {
-    openCart();
-  });
+const productPriceLabel = document.getElementById('product-price-cart');
+const productQtyLabel = document.getElementById('product-qty-cart');
+const productTotalLabel = document.getElementById('product-total-cart');
 
-  btnIncrementItem.addEventListener('click', () => {
-    if (!inputItemQty.value) {
-      inputItemQty.value = 0;
-    }
+const productPrice = 125.0;
 
-    if (parseInt(inputItemQty.value) >= 100) {
-      inputItemQty.value = 100;
-    } else {
-      inputItemQty.value = parseInt(inputItemQty.value) + 1;
-    }
-  });
-
-  btnDecrementItem.addEventListener('click', () => {
-    if (!inputItemQty.value) {
-      inputItemQty.value = 0;
-    }
-
-    if (parseInt(inputItemQty.value) <= 0) {
-      inputItemQty.value = 0;
-    } else {
-      inputItemQty.value = parseInt(inputItemQty.value) - 1;
-    }
-  });
+function setItemQty(n) {
+  inputItemQty.value = n;
+  productQtyLabel.innerText = inputItemQty.value;
 }
+
+function removeFromCart() {
+  setItemQty(0);
+  updateTotalLabel();
+}
+
+function updateTotalLabel() {
+  const total = inputItemQty.value * productPrice;
+  const totalFixed = Number.parseFloat(total).toFixed(2).toString();
+  productTotalLabel.innerText = totalFixed;
+}
+
+function incrementItem() {
+  if (!inputItemQty.value) {
+    setItemQty(0);
+  }
+
+  if (parseInt(inputItemQty.value) >= 100) {
+    setItemQty(100);
+  } else {
+    setItemQty(parseInt(inputItemQty.value) + 1);
+  }
+
+  updateTotalLabel();
+}
+
+function decrementItem() {
+  if (!inputItemQty.value) {
+    setItemQty(0);
+  }
+
+  if (parseInt(inputItemQty.value) <= 0) {
+    setItemQty(0);
+  } else {
+    setItemQty(parseInt(inputItemQty.value) - 1);
+  }
+
+  updateTotalLabel();
+}
+
+export default function initAddToCartButton() {
+  btnAddToCart.addEventListener('click', openCart);
+  btnIncrementItem.addEventListener('click', incrementItem);
+  btnDecrementItem.addEventListener('click', decrementItem);
+}
+
+productPriceLabel.innerText = Number.parseFloat(productPrice).toFixed(2);
