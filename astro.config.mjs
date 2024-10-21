4b72461d8fbf04a3c1e81b77e7825a7b44f7316b
astn <github@msg.austinh.io>
2024-10-21 15:51:04 -0700

    Adjusted base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://github.astn.io/',
-  base: 'frontend-mentor-ecommerce-product-page',
+  base: '/frontend-mentor-ecommerce-product-page',
 });
