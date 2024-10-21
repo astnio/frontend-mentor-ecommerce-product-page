ebd79d228d8a12f04d935d8bb492354b9fc28fe8
astn <github@msg.austinh.io>
2024-10-21 16:06:30 -0700

    Updated basepath to all source links

--- src/scripts/imagePicker.mjs
+++ src/scripts/imagePicker.mjs
@@ -1,9 +1,9 @@
 class ImagePicker {
   static imageMainSources = [
-    '/src/assets/images/image-product-1.jpg',
-    '/src/assets/images/image-product-2.jpg',
-    '/src/assets/images/image-product-3.jpg',
-    '/src/assets/images/image-product-4.jpg',
+    '/frontend-mentor-ecommerce-product-page/src/assets/images/image-product-1.jpg',
+    '/frontend-mentor-ecommerce-product-page/src/assets/images/image-product-2.jpg',
+    '/frontend-mentor-ecommerce-product-page/src/assets/images/image-product-3.jpg',
+    '/frontend-mentor-ecommerce-product-page/src/assets/images/image-product-4.jpg',
   ];
 
   static overlayTracker = {
