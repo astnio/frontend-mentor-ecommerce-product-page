c8af51920b0676effd7db84426ccfca89cb97606
astn <github@msg.austinh.io>
2024-10-22 12:33:23 -0700

    Moved drawer logic into drawer component

--- src/scripts/navDrawerManager.mjs
+++ src/scripts/navDrawerManager.mjs
@@ -1,21 +1,21 @@
-const navDrawer = document.getElementById('nav-drawer');
-const drawerOverlay = document.getElementById('nav-drawer-overlay');
-const btnCloseDrawer = document.getElementById('btn-close-nav-drawer');
-const btnOpenDrawer = document.getElementById('btn-open-nav-drawer');
+// const navDrawer = document.getElementById('nav-drawer');
+// const drawerOverlay = document.getElementById('nav-drawer-overlay');
+// const btnCloseDrawer = document.getElementById('btn-close-nav-drawer');
+// const btnOpenDrawer = document.getElementById('btn-open-nav-drawer');
 
-export default function initDrawerButtons() {
-  btnCloseDrawer.addEventListener('click', () => {
-    navDrawer.dataset.active = 'false';
-    drawerOverlay.dataset.active = 'false';
-  });
+// export default function initDrawerButtons() {
+//   btnCloseDrawer.addEventListener('click', () => {
+//     navDrawer.dataset.active = 'false';
+//     drawerOverlay.dataset.active = 'false';
+//   });
 
-  btnOpenDrawer.addEventListener('click', () => {
-    navDrawer.dataset.active = 'true';
-    drawerOverlay.dataset.active = 'true';
-  });
+//   btnOpenDrawer.addEventListener('click', () => {
+//     navDrawer.dataset.active = 'true';
+//     drawerOverlay.dataset.active = 'true';
+//   });
 
-  drawerOverlay.addEventListener('click', () => {
-    navDrawer.dataset.active = 'false';
-    drawerOverlay.dataset.active = 'false';
-  });
-}
+//   drawerOverlay.addEventListener('click', () => {
+//     navDrawer.dataset.active = 'false';
+//     drawerOverlay.dataset.active = 'false';
+//   });
+// }
